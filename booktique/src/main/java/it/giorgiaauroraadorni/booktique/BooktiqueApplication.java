fe02262c982de2fb9a35477f36aaf8b1a377a383
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-08 16:53:54 +0100

    Created Booktique project

---
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/BooktiqueApplication.java
@@ -0,0 +1,12 @@
+package it.giorgiaauroraadorni.booktique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooktiqueApplication.class, args);
+    }
+}
