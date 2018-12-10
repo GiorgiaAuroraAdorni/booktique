4874c856f07c6cac431d6aaff97c7be27829288c
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-10 20:34:06 +0100

    Modified to main class:
    Spring MVC uses it to handle Web requests.
    The home method sends a simple 'Hello World' response.

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/BooktiqueApplication.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/BooktiqueApplication.java
@@ -3,10 +3,18 @@ package it.giorgiaauroraadorni.booktique;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RestController
 public class BooktiqueApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BooktiqueApplication.class, args);
