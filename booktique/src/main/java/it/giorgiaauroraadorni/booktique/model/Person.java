0dafcd06d2bc5396261e4ad58201967bba86b022
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-11 20:46:51 +0100

    Created Person superclass
    the person entity is mapped to a table named person in the database

---
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Person.java
@@ -0,0 +1,83 @@
+package it.giorgiaauroraadorni.booktique.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "person")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Person extends AuditModel {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 16, max = 16)
+    private String CF;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String surname;
+
+    @Size(min = 1, max = 70)
+    private String email;
+
+    @Size(min = 1, max = 25)
+    private String phone;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCF() {
+        return CF;
+    }
+
+    public void setCF(String CF) {
+        this.CF = CF;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surnanme) {
+        this.surname = surnanme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
