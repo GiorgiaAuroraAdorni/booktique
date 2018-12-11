a16923d2c7f42b038c8f86687be6e33f29ceaf83
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-11 21:31:26 +0100

    Created Customer class extension of Person

---
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Customer.java
@@ -0,0 +1,64 @@
+package it.giorgiaauroraadorni.booktique.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="customer")
+public class Customer extends Person {
+
+    private Person customer;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    private String vatNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    // Getters and Setters
+    public Person getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Person customer) {
+        this.customer = customer;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    public void setVatNumber(String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
