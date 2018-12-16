7a78550c2cc65313e6b69c2cf450f9c77f88a8d2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-16 17:43:09 +0100

    Added field biography and web site url for the authors

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Author.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Author.java
@@ -1,12 +1,19 @@
 package it.giorgiaauroraadorni.booktique.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name="authors")
 public class Author extends Person {
 
     private Person author;
+
+    private String biography;
+
+    // The web site url matches with or without protocol.
+    @Pattern(regexp = "^[http://www.|https://www.|www.][\\S]+$")
+    private String webSiteURL;
 
     // Getters and Setters
     public Person getAuthor() {
@@ -15,5 +22,21 @@ public class Author extends Person {
 
     public void setAuthor(Person author) {
         this.author = author;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getWebSiteURL() {
+        return webSiteURL;
+    }
+
+    public void setWebSiteURL(String webSiteURL) {
+        this.webSiteURL = webSiteURL;
     }
 }
