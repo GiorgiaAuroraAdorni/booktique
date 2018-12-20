37c2ef21514f5b6722a401bba30f982bf7fc9200
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-12-20 09:25:46 +0100

    Second test that check the creation of a book with an author and with the other paramerts

--- booktique/src/test/java/it/giorgiaauroraadorni/booktique/BookRepositoryTest.java
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/BookRepositoryTest.java
@@ -1,12 +1,17 @@
 package it.giorgiaauroraadorni.booktique;
 
+import it.giorgiaauroraadorni.booktique.model.Author;
 import it.giorgiaauroraadorni.booktique.model.Book;
+import it.giorgiaauroraadorni.booktique.repository.AuthorRepository;
 import it.giorgiaauroraadorni.booktique.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,6 +20,9 @@ class BookRepositoryTest {
     // Set automatically the attribute to the BookRepository instance
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
 
     @Test
     void repositoryLoads() {}
@@ -41,6 +49,40 @@ class BookRepositoryTest {
         assertNotNull(book.getUpdatedAt());
     }
 
+    @Test
+    public void testCompleteBookCreate() {
+        // create an author
+        Author author = new Author();
+        Set<Author> authors = new HashSet<Author>();
+        authors.add(author);
+
+        author.setFiscalCode("CRLZNR64P25A638G");
+        author.setName("Carlos");
+        author.setSurname("Ruiz Zafón");
+        author.setWebSiteURL("http://www.carlosruizzafon.com");
+
+        // save the author in the repository
+        authorRepository.save(author);
+
+        // add also the author to the book attributes
+        Book book = new Book();
+        book.setIsbn("9788408081180");
+        book.setTitle("El juego del ángel");
+        book.setPublisher("Editorial Planeta");
+        book.setAuthors(authors);
+        book.setBookFormat(Book.Format.hardcover);
+        book.setEdition(1);
+        book.setLanguage("español");
+        book.setPublicationDate(LocalDate.of(2008, 04, 17));
+
+        // save the book in the repository
+        bookRepository.save(book);
+
+        // check if the book is added correctly to the repository
+        assertTrue(bookRepository.findAll().contains(book));
+        assertNotNull(book.getAuthors());
+        assertTrue(book.getAuthors().contains(author));
+        assertNotNull(author.getId());
     }
 
 }
