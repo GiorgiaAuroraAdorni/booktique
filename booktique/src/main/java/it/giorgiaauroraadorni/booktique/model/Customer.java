e4d535d5180f2ef3e5131c5571f5cee9944a4575
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-03 21:33:25 +0100

    Fixed relationship between addresses and customers

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Customer.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Customer.java
@@ -21,7 +21,7 @@ public class Customer extends Person {
     private String vatNumber;
 
     // This is a full postal address for the contact represented by this object.
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Address address;
 
     // Getters and Setters
