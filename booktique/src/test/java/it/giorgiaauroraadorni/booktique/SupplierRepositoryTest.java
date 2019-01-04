1501f6b3f78710c5e02c36b7d022f9e4e42b1390
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-04 11:21:16 +0100

    Created SupplierRepositoryTest

---
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/SupplierRepositoryTest.java
@@ -0,0 +1,15 @@
+package it.giorgiaauroraadorni.booktique;
+
+import it.giorgiaauroraadorni.booktique.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class SupplierRepositoryTest {
+
+    // Set automatically the attribute to the supplierRepository instance
+    @Autowired
+    private SupplierRepository supplierRepository;
+}
