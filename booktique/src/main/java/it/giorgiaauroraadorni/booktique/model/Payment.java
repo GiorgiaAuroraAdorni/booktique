cdda756102b90d6de21795fd77b24d5b288d477e
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-04 23:03:03 +0100

    Transformed embeddable to entity and added the primary key id

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Payment.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/model/Payment.java
@@ -1,16 +1,20 @@
 package it.giorgiaauroraadorni.booktique.model;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.validation.constraints.NotNull;
+import javax.persistence.*;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
-@Embeddable
-public class Payment {
+@Entity
+@Table(name="payments")
+public class Payment extends AuditModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
 
     // This is a Card Number. The format allows 16 numbers in groups of 4 separated by -, ,or nothing.
-    @Column(nullable = false)
+    @Column(nullable = false, length = 16)
+    @Size(min = 16)
     @Pattern(regexp = "^(\\d{4}-){3}\\d{4}$|^(\\d{4} ){3}\\d{4}$|^\\d{16}$")
     private String cardNumber;
 
@@ -22,13 +26,22 @@ public class Payment {
     private LocalDate expireDate;
 
     // This is a Card Verification Codes. The format allows 3 or 4 numerical digits.
-    @Column(nullable = false)
+    @Column(nullable = false, length = 4)
+    @Size(min = 3)
     @Pattern(regexp = "\\d{3,4}$")
     private String CVC;
 
     private LocalDate paymentDate;
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getCardNumber() {
         return cardNumber;
     }
