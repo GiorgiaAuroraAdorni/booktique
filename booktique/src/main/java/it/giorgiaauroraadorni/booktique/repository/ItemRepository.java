14cfa25589be1624e0614f0cca807baeef691069
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-04 12:42:13 +0100

    Created ItemRepository

---
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package it.giorgiaauroraadorni.booktique.repository;
+
+import it.giorgiaauroraadorni.booktique.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
