10ed44df37229836da4d718aad7567d666090eb7
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-05 12:31:52 +0100

    Removed unused imports

--- booktique/src/test/java/it/giorgiaauroraadorni/booktique/PurchaseRepositoryTest.java
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/PurchaseRepositoryTest.java
@@ -11,10 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
