c434cd7f2482da4f961f079efc25c8fddbb9a898
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-07 23:58:21 +0100

    Added methods in AuthorRepository that implement findBy

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/AuthorRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/AuthorRepository.java
@@ -4,6 +4,16 @@ import it.giorgiaauroraadorni.booktique.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long>{
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    List<Author> findByName(String name);
+
+    List<Author> findBySurname(String surname);
+
+    List<Author> findByFiscalCode(String fiscalCode);
+
+    List<Author> findByEmail(String fiscalCode);
 }
