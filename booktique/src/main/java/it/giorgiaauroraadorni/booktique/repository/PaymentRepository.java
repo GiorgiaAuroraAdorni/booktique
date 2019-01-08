d7d34f26a41f296c5780007218634d6317f38fac
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 10:32:36 +0100

    Added search operations in PaymentRepository

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/PaymentRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/PaymentRepository.java
@@ -4,7 +4,13 @@ import it.giorgiaauroraadorni.booktique.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // implements search operations
+    List<Payment> findByCardholderName(String cardholderName);
 
+    List<Payment> findByPaymentDate(LocalDate paymentDate);
 }
