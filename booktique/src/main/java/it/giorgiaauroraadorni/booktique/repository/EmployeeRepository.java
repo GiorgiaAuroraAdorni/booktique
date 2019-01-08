b7666b471352a334779bbc597cf54c05bc36adce
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 14:27:25 +0100

    Added search operations in EmployeeRepository

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/EmployeeRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/EmployeeRepository.java
@@ -4,7 +4,18 @@ import it.giorgiaauroraadorni.booktique.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // implements search operations
+    List<Employee> findByName(String name);
 
+    List<Employee> findBySurname(String surname);
+
+    List<Employee> findByFiscalCode(String fiscalCode);
+
+    List<Employee> findByUsername(String username);
+
+    List<Employee> findBySupervisor(Employee supervisor);
 }
