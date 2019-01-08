bf96c09e50e3442f1a98b6ee4dd8385d43458fed
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 09:28:49 +0100

    Added search operations (via findBy) in SupplierRepository

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/SupplierRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/SupplierRepository.java
@@ -4,7 +4,12 @@ import it.giorgiaauroraadorni.booktique.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    // implements search operations
+    List<Supplier> findByCompanyName(String companyName);
 
+    List<Supplier> findByEmail(String email);
 }
