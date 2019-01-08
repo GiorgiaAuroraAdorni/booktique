4d7240fd67ea2f7f08b9fe26e1bfbc092a0bb415
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 14:47:41 +0100

    Implemented only a search operation that finds Item via findByBookItem

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/ItemRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/ItemRepository.java
@@ -1,9 +1,14 @@
 package it.giorgiaauroraadorni.booktique.repository;
 
+import it.giorgiaauroraadorni.booktique.model.Book;
 import it.giorgiaauroraadorni.booktique.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    // implements search operations
+    List<Item> findByBookItem(Book bookItem);
 }
