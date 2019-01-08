b2688a6083e895763d4fb9cacb6ec2f5a65ae7f2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 14:36:58 +0100

    Implemented search operations in CustomerRepository

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/CustomerRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/CustomerRepository.java
@@ -4,6 +4,16 @@ import it.giorgiaauroraadorni.booktique.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    // implements search operations
+    List<Customer> findByName(String name);
+
+    List<Customer> findBySurname(String surname);
+
+    List<Customer> findByFiscalCode(String fiscalCode);
+
+    List<Customer> findByUsername(String username);
 }
