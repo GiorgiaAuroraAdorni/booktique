76ef4f20683dfff042873e924a5dabf8cb640b78
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 14:32:56 +0100

    Added other attributes in AuthorTestFactory createValidEntity

--- booktique/src/test/java/it/giorgiaauroraadorni/booktique/model/AuthorTestFactory.java
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/model/AuthorTestFactory.java
@@ -3,8 +3,6 @@ package it.giorgiaauroraadorni.booktique.model;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class AuthorTestFactory implements EntityTestFactory<Author> {
@@ -13,10 +11,19 @@ public class AuthorTestFactory implements EntityTestFactory<Author> {
     public Author createValidEntity(int idx) {
         var author = new Author();
 
-        // creates authors with only the mandatory parameter (inherits from person)
+        // mandatory attribute (inherits from person)
         author.setFiscalCode("CGNNMO00T00L00" + idx + "S");
-        author.setName("Nome " + idx);
-        author.setSurname("Cognome " +idx);
+        author.setName("Nome" + idx);
+        author.setSurname("Cognome" +idx);
+
+        // other attributes
+        author.setDateOfBirth(LocalDate.now().minusYears(10 + idx));
+        author.setEmail(author.getName() + author.getSurname() + "@mail.com");
+        author.setMobilePhone("333333333" + idx);
+        author.setWebSiteURL("https://www." + author.getName() + author.getSurname() + ".org");
+        author.setBiography(author.getName() + " is a friendly government politician and has a post-graduate degree " +
+                "in philosophy, politics and economics. To lear more about " + author.getName() + " " +
+                author.getSurname() + " by visit the website " + author.getWebSiteURL() + " !");
 
         return author;
     }
