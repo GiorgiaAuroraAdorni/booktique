71d2b3d237ec7b981574e660371909224bdda5d2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 14:45:33 +0100

    Added comment to AddressRepository

--- booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/AddressRepository.java
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/repository/AddressRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    // no search operations implemented for this entity
 }
