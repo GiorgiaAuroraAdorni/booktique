7446284506ddda72a7ab8ef317e693a441b3dee5
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-08 23:47:06 +0100

    Created CustomerTestFactory component with createValidEntity and updateValidEntity methods and updated CustomerRepositoryTest according to that.
    moved repositoryFindAll() in the correct section

---
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/model/CustomerTestFactory.java
@@ -0,0 +1,39 @@
+package it.giorgiaauroraadorni.booktique.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class CustomerTestFactory implements EntityTestFactory<Customer> {
+    @Autowired
+    private EntityTestFactory<Address> addressFactory;
+
+    @Override
+    public Customer createValidEntity(int idx) {
+        var customer = new Customer();
+        var address = addressFactory.createValidEntity();
+
+        // mandatory attribute
+        customer.setFiscalCode("CGNNMO00T00L00" + idx + "S");
+        customer.setName("Nome" + idx);
+        customer.setSurname("Cognome" +idx);
+        customer.setUsername("UserNo" + idx);
+        customer.setPassword("Qwerty1234");
+        customer.setAddress(address);
+
+        // other attributes
+        customer.setDateOfBirth(LocalDate.now().minusYears(20 + idx));
+        customer.setEmail(customer.getName() + customer.getSurname() + "@mail.com");
+        customer.setMobilePhone("333111111" + idx);
+        customer.setVatNumber("IT10000000000");
+
+        return customer;
+    }
+
+    @Override
+    public void updateValidEntity(Customer entity) {
+
+    }
+}
--- booktique/src/test/java/it/giorgiaauroraadorni/booktique/repository/CustomerRepositoryTest.java
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/repository/CustomerRepositoryTest.java
@@ -2,20 +2,20 @@ package it.giorgiaauroraadorni.booktique.repository;
 
 import it.giorgiaauroraadorni.booktique.model.Address;
 import it.giorgiaauroraadorni.booktique.model.Customer;
+import it.giorgiaauroraadorni.booktique.model.EntityTestFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolationException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,111 +29,27 @@ class CustomerRepositoryTest {
     @Autowired
     private AddressRepository addressRepository;
 
+    @Autowired
+    private EntityTestFactory<Customer> customerFactory;
+
+    @Autowired
+    private EntityTestFactory<Address> addressFactory;
+
     private List<Customer> dummyCustomers;
-    private List<Address> dummyAddresses;
-
-    /**
-     * Create a list of addresses entities that will be use in the test
-     */
-    private void createDummyAddress() {
-        dummyAddresses = IntStream
-                .range(0, 3)
-                .mapToObj(i -> new Address())
-                .collect(Collectors.toList());
-
-        // create an address with only the mandatory parameter
-        dummyAddresses.get(0).setStreetAddress("Via Vinicio 59");
-        dummyAddresses.get(0).setCity("Montecassiano");
-        dummyAddresses.get(0).setProvince("MC");
-        dummyAddresses.get(0).setPostalCode("04017");
-        dummyAddresses.get(0).setCountry("Italia");
-
-        // create an address with all the possible attributes
-        dummyAddresses.get(1).setStreetAddress("Via Tancredi 96");
-        dummyAddresses.get(1).setCity("Fonteblanda");
-        dummyAddresses.get(1).setProvince("GR");
-        dummyAddresses.get(1).setRegion("Toscana");
-        dummyAddresses.get(1).setPostalCode("32349");
-        dummyAddresses.get(1).setCountry("Italia");
-        dummyAddresses.get(1).setBuilding("Appartamento 62 De Santis del friuli");
-
-        // create an address with all the possible attributes
-        dummyAddresses.get(2).setStreetAddress("Via Leone 1");
-        dummyAddresses.get(2).setCity("Milano");
-        dummyAddresses.get(2).setProvince("MI");
-        dummyAddresses.get(2).setPostalCode("41845");
-        dummyAddresses.get(2).setCountry("Italia");
-        dummyAddresses.get(2).setBuilding("Piano 8");
-
-        // save the addresses in the repository
-        dummyAddresses = addressRepository.saveAll(dummyAddresses);
-    }
-
-    /**
-     * Create a list of customers entities that will be use in the test
-     */
-    private void createDummyCustomer() {
-        dummyCustomers = IntStream
-                .range(0, 3)
-                .mapToObj(i -> new Customer())
-                .collect(Collectors.toList());
-
-        // create a customer with only the mandatory parameter (inherited from person)
-        dummyCustomers.get(0).setFiscalCode("MTCKLN83C13G224W");
-        dummyCustomers.get(0).setName("Kaitlin");
-        dummyCustomers.get(0).setSurname("Mitchell");
-        Address address = dummyAddresses.get(0);
-        dummyCustomers.get(0).setAddress(address);
-        dummyCustomers.get(0).setUsername("KaitlinMitchell83");
-        dummyCustomers.get(0).setPassword("W422g31C38nLkCtM");
-
-        // create a customer with all the person attributes
-        dummyCustomers.get(1).setFiscalCode("DVSMGN49A01C933C");
-        dummyCustomers.get(1).setName("Morgan");
-        dummyCustomers.get(1).setSurname("Davison");
-        dummyCustomers.get(1).setAddress(dummyAddresses.get(1));
-        dummyCustomers.get(1).setUsername("MorganDavison49");
-        dummyCustomers.get(1).setPassword("C339c10A94nGmSvD");
-        dummyCustomers.get(1).setDateOfBirth(LocalDate.of(1949, 1, 1));
-        dummyCustomers.get(1).setEmail("MorganDavidson@mail.com");
-        dummyCustomers.get(1).setMobilePhone("+393733733730");
-
-        // create a customer with many attributes
-        dummyCustomers.get(2).setFiscalCode("FRSTVS80T12A271K");
-        dummyCustomers.get(2).setName("Travis");
-        dummyCustomers.get(2).setSurname("Frost");
-        dummyCustomers.get(2).setAddress(dummyAddresses.get(2));
-        dummyCustomers.get(2).setDateOfBirth(LocalDate.of(1980, 12, 12));
-        dummyCustomers.get(2).setEmail("TravisFrost@mail.com");
-        dummyCustomers.get(2).setMobilePhone("3263261001");
-        dummyCustomers.get(2).setUsername("TravisFrost80");
-        dummyCustomers.get(2).setPassword("K172a21T08sVtSrF");
-        dummyCustomers.get(2).setVatNumber("12345678901");
-
-        // save the customers in the repository
-        dummyCustomers = customerRepository.saveAll(dummyCustomers);
-    }
 
     @BeforeEach
-    void createDummyEntities() {
-        createDummyAddress();
-        createDummyCustomer();
+    void createDummyCustomers() {
+        // create a list of valid customers entities
+        dummyCustomers = customerFactory.createValidEntities(2);
+
+        // save the created entities in the customerRepository and persist addresses
+        dummyCustomers = customerRepository.saveAll(dummyCustomers);
     }
 
     /* Test CRUD operations */
 
     @Test
     void repositoryLoads() {}
-
-    @Test
-    void repositoryFindAll() {
-        var savedCustomers = customerRepository.findAll();
-        var savedAddresses = addressRepository.findAll();
-
-        // check if all the customers are correctly added to the repository
-        assertTrue(savedCustomers.containsAll(dummyCustomers), "findAll should fetch all dummy customers");
-        assertTrue(savedAddresses.containsAll(dummyAddresses), "findAll should fetch all dummy addresses");
-    }
 
     /**
      * Insert many entries in the repository and check if these are readable and the attributes are correct
@@ -183,24 +99,24 @@ class CustomerRepositoryTest {
         Customer savedCustomer = customerRepository.findById(dummyCustomers.get(0).getId()).get();
 
         // change some attributes
+        Address newAddress = addressFactory.createValidEntity(1);
+        savedCustomer.setAddress(newAddress);
         savedCustomer.setName("Terry");
-        savedCustomer.setFiscalCode("MTCTRR83C13G224W");
         savedCustomer.setUsername("TerryMitchell83");
         savedCustomer.setPassword("W422g31C38rRtCtM");
-        savedCustomer.setAddress(dummyAddresses.get(1));
 
         // update the Customer object
-        customerRepository.save(savedCustomer);
+        savedCustomer = customerRepository.saveAndFlush(savedCustomer);
         Customer updatedCustomer = customerRepository.findById(savedCustomer.getId()).get();
 
         // check that all the attributes have been updated correctly and contain the expected value
         assertNotNull(updatedCustomer);
+        assertNotNull(updatedCustomer.getAddress());
         assertEquals(savedCustomer, updatedCustomer);
         assertEquals("Terry", updatedCustomer.getName());
-        assertEquals("MTCTRR83C13G224W", updatedCustomer.getFiscalCode());
         assertEquals("TerryMitchell83", updatedCustomer.getUsername());
         assertEquals("W422g31C38rRtCtM", updatedCustomer.getPassword());
-        assertEquals(addressRepository.getOne(dummyAddresses.get(1).getId()), updatedCustomer.getAddress());
+        assertEquals(newAddress, updatedCustomer.getAddress());
     }
 
     /**
@@ -209,19 +125,11 @@ class CustomerRepositoryTest {
      */
     @Test
     public void testUniqueCustomerUsernameIdentifier() {
-        Customer duplicatedUser = new Customer();
+        Customer savedCustomer = customerRepository.findById(dummyCustomers.get(0).getId()).get();
+        var duplicatedUser = customerFactory.createValidEntity();
 
-        // set manually a new id in order to insert a new record and not for update an existing record
-        duplicatedUser.setId(9999l);
-        duplicatedUser.setFiscalCode("MTCKLN83C18G224W");
-        duplicatedUser.setName("Kaitlin Christie");
-        duplicatedUser.setSurname("Mitchell");
-        duplicatedUser.setAddress(dummyAddresses.get(0));
-        duplicatedUser.setPassword("W422g31C38nLkCtM");
-
-        // save the customer in the repository
         assertThrows(DataIntegrityViolationException.class, () -> {
-            duplicatedUser.setUsername("KaitlinMitchell83");
+            duplicatedUser.setUsername(savedCustomer.getUsername());
             customerRepository.saveAndFlush(duplicatedUser);
         });
     }
@@ -239,46 +147,53 @@ class CustomerRepositoryTest {
     }
 
     /**
-     * Throws an exception when attempting to create or update a customer with illegal size for the attributes
+     * Throws an exception when attempting to create or update a customer with illegal size for the username attribute
      */
     @Test
-    public void testIllegalSizeAttributes() {
-        Customer invalidCustomer = new Customer();
-
-        invalidCustomer.setFiscalCode("CRLCHR83C13G224W");
-        invalidCustomer.setName("Christie");
-        invalidCustomer.setSurname("Carlson");
-        invalidCustomer.setAddress(addressRepository.getOne(dummyAddresses.get(0).getId()));
-        invalidCustomer.setUsername("ChristieCarlson83");
-        invalidCustomer.setPassword("W422g31C38rHcLrC");
-
-        customerRepository.save(invalidCustomer);
+    public void testIllegalUsernameSize() {
+        var invalidCustomer = customerFactory.createValidEntity();
 
         assertThrows(DataIntegrityViolationException.class, () -> {
             invalidCustomer.setUsername("ChristieCarlsonClark15gennaio1983");
             customerRepository.saveAndFlush(invalidCustomer);
-        });
+        }, "Username cannot be longer than 32 characters");
 
-        assertThrows(ConstraintViolationException.class, () -> {
-            invalidCustomer.setUsername("Chri");
+        assertThrows(JpaSystemException.class, () -> {
+            invalidCustomer.setUsername("User");
             customerRepository.saveAndFlush(invalidCustomer);
-        });
+        }, "Username must be at least 5 characters long");
+    }
 
-        assertThrows(ConstraintViolationException.class, () -> {
+    /**
+     * Throws an exception when attempting to create or update a customer with illegal size for the password attribute
+     */
+    @Test
+    public void testIllegalPasswordSize() {
+        var invalidCustomer = customerFactory.createValidEntity();
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
             invalidCustomer.setPassword("-X2LPM4r`2.SJn)nGxW3Dt}4$C+z??\"d7np=fHWDTB`y2ye:w2>\\5Kf,}\\Ks?*NBq7FG./Qp" +
                     "(>uxFtfs~U(A!tLHSGk>a5bhue^2wq#~3K9mc2[P(J:]c&hez(Jm&F?j2");
             customerRepository.saveAndFlush(invalidCustomer);
-        });
+        }, "Password cannot be longer than 128 characters");
 
-        assertThrows(ConstraintViolationException.class, () -> {
-            invalidCustomer.setPassword("Chris83");
+        assertThrows(JpaSystemException.class, () -> {
+            invalidCustomer.setPassword("Qwerty12");
             customerRepository.saveAndFlush(invalidCustomer);
-        });
+        }, "Password must be at least 8 characters long");
+    }
+
+    /**
+     * Throws an exception when attempting to create or update a customer with illegal size for the vat number attribute
+     */
+    @Test
+    public void testIllegalVatNumberSize() {
+        var invalidCustomer = customerFactory.createValidEntity();
 
         assertThrows(ConstraintViolationException.class, () -> {
             invalidCustomer.setVatNumber("12345678901234567890");
             customerRepository.saveAndFlush(invalidCustomer);
-        });
+        }, "Vat Number must be at least 11 digit characters long");
     }
 
     /**
@@ -300,7 +215,42 @@ class CustomerRepositoryTest {
         assertTrue(customerRepository.findAll().isEmpty());
     }
 
+    /**
+     * Throws an exception when attempting to delete the customer address
+     */
+    @Test
+    public void testDeleteCustomerAddress() {
+        // get a customer and his address from the repository
+        Customer savedCustomer = customerRepository.findById(dummyCustomers.get(0).getId()).get();
+        Address customerAddress = savedCustomer.getAddress();
+
+        // delete the address object
+        addressRepository.delete(customerAddress);
+
+        // check that the address has been deleted correctly
+        assertEquals(addressRepository.findById(customerAddress.getId()), Optional.empty());
+
+        // throws an exception when attempting to access to a customer object whose address has been deleted
+        assertThrows(AssertionFailedError.class, () -> {
+            assertNull(customerRepository.findById(savedCustomer.getId()).get().getAddress());
+            assertNotEquals(customerAddress, customerRepository.findById(savedCustomer.getId()).get().getAddress());
+        }, "It's not possible to eliminate a customer address");
+    }
+
     /* Test search operations */
+
+    @Test
+    void repositoryFindAll() {
+        var savedCustomers = customerRepository.findAll();
+        var savedAddresses = addressRepository.findAll();
+
+        // check if all the customers are correctly added to the repository
+        assertTrue(savedCustomers.containsAll(dummyCustomers), "findAll should fetch all dummy customers");
+        assertFalse(savedAddresses.isEmpty());
+        for (Customer c : dummyCustomers) {
+            assertTrue(savedAddresses.contains(c.getAddress()), "findAll should fetch all dummy addresses");
+        }
+    }
 
     @Test
     public void testFindById() {
