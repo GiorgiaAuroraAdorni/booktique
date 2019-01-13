b1dea1fcd217d6378e1757ab601f1d02260cbb8c
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-01-13 12:45:54 +0100

    Moved method associationEquals out of test and added documentation

---
+++ booktique/src/main/java/it/giorgiaauroraadorni/booktique/utility/Associations.java
@@ -0,0 +1,35 @@
+package it.giorgiaauroraadorni.booktique.utility;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Associations {
+    /**
+     * Compare to set of entity instances that represent the associations.
+     * Return {@code true} is the associations are equal to each other and {@code false} otherwise.
+     * During the comparison is considered all the attributes of the entities. To do that is use a dictionary
+     * containing the fields of the reference entities.
+     * @param expected set of entity instances.
+     * @param actual set of entity instances.
+     * @param optionalId if {@code true} the entities identifier is included in the comparison, otherwise no.
+     * @param <T> the type of entities to compare.
+     * @return {@code true} if the associations are equal to each other and {@code false} otherwise
+     */
+    public static <T extends EntityToDict> boolean associationEquals(Set<T> expected, Set<T> actual,
+                                                                     boolean optionalId) {
+        if (expected == actual) return true;
+        if (expected == null || actual == null) return false;
+
+        var dictsExpected = expected
+                .stream()
+                .map((e) -> e.entityToDict(optionalId))
+                .collect(Collectors.toSet());
+
+        var dictsActual = actual
+                .stream()
+                .map((e) -> e.entityToDict(optionalId))
+                .collect(Collectors.toSet());
+
+        return dictsExpected.equals(dictsActual);
+    }
+}
--- booktique/src/test/java/it/giorgiaauroraadorni/booktique/utility/Assertions.java
+++ booktique/src/test/java/it/giorgiaauroraadorni/booktique/utility/Assertions.java
@@ -1,12 +1,21 @@
 package it.giorgiaauroraadorni.booktique.utility;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
+import static it.giorgiaauroraadorni.booktique.utility.Associations.associationEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 public class Assertions {
+    /**
+     *
+     * @param expected
+     * @param actual
+     * @param optionalId
+     * @param <T>
+     */
+    public static <T extends EntityToDict> void assertAssociationEquals(Set<T> expected, Set<T> actual, boolean optionalId) {
+        assertTrue(associationEquals(expected, actual, optionalId));
+    }
 
     /**
      *
@@ -14,30 +23,7 @@ public class Assertions {
      * @param actual
      * @param optionalId
      * @param <T>
-     * @return
      */
-    public static <T extends EntityToDict> boolean associationEquals(Set<T> expected, Set<T> actual,
-                                                                     boolean optionalId) {
-        if (expected == actual) return true;
-        if (expected == null || actual == null) return false;
-
-        var dictsExpected = expected
-                .stream()
-                .map((e) -> e.entityToDict(optionalId))
-                .collect(Collectors.toSet());
-
-        var dictsActual = actual
-                .stream()
-                .map((e) -> e.entityToDict(optionalId))
-                .collect(Collectors.toSet());
-
-        return dictsExpected.equals(dictsActual);
-    }
-
-    public static <T extends EntityToDict> void assertAssociationEquals(Set<T> expected, Set<T> actual, boolean optionalId) {
-        assertTrue(associationEquals(expected, actual, optionalId));
-    }
-
     public static <T extends EntityEqualsByAttributes> void assertAttributesEquals(T expected, T actual, boolean optionalId) {
         assertTrue(expected.equalsByAttributes(actual, optionalId));
     }
